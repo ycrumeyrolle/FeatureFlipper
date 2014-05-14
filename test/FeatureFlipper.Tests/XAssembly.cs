0996af3c5e779ffdad733a8aec912e63fdf7f5d8
Yann Crumeyrolle <murk@club-internet.fr>
2014-05-14 20:56:28 +0200

    Fix unit test with XAssembly serialization

--- test/FeatureFlipper.Tests/XAssembly.cs
+++ test/FeatureFlipper.Tests/XAssembly.cs
@@ -2,11 +2,16 @@
 {
     using System;
     using System.Reflection;
+    using System.Runtime.Serialization;
 
     [Serializable]
     public class XAssembly : Assembly
     {
         public XAssembly()
+        {               
+        }
+
+        public XAssembly(SerializationInfo info, StreamingContext context)
         {
         }
     }
