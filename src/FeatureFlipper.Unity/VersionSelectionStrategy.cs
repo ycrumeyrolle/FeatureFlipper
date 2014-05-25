286fbb5abffeac58ae2a5d68353c3233a4e8c7a7
Yann Crumeyrolle <murk@club-internet.fr>
2014-05-25 23:57:16 +0200

    Unit test on VersionSelectionStrategy

---
+++ src/FeatureFlipper.Unity/VersionSelectionStrategy.cs
@@ -0,0 +1,71 @@
+﻿namespace FeatureFlipper.Unity
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Practices.ObjectBuilder2;
+
+    /// <summary>
+    /// A <see cref="BuilderStrategy"/> that handles feature versions toggle.
+    /// </summary>
+    public sealed class VersionSelectionStrategy : BuilderStrategy
+    {
+        private readonly IFeatureFlipper flipper;
+
+        private readonly FeatureNameProvider nameProvider = new FeatureNameProvider();
+
+        private readonly IDictionary<Type, IDictionary<Type, string>> featureVersionMapping;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FlippingBuilderStrategy"/> class.
+        /// </summary>
+        /// <param name="flipper">The <see cref="IFeatureFlipper"/>.</param>
+        /// <param name="featureVersionMapping">The from-to-verion type mapping.</param>
+        public VersionSelectionStrategy(IFeatureFlipper flipper, IDictionary<Type, IDictionary<Type, string>> featureVersionMapping)
+        {
+            if (flipper == null)
+            {
+                throw new ArgumentNullException("flipper");
+            }
+
+            if (featureVersionMapping == null)
+            {
+                throw new ArgumentNullException("featureVersionMapping");
+            }
+
+            this.flipper = flipper;
+            this.featureVersionMapping = featureVersionMapping;
+        }
+
+        /// <inheritsdoc/>
+        public override void PreBuildUp(IBuilderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            Type fromType = context.OriginalBuildKey.Type;
+
+            string featureName = this.nameProvider.GetFeatureName(context.BuildKey.Type);
+            bool isOn;
+            if (context.Existing == null)
+            {
+                IDictionary<Type, string> mapping;
+                if (this.featureVersionMapping.TryGetValue(fromType, out mapping))
+                {
+                    foreach (var item in mapping)
+                    {
+                        featureName = this.nameProvider.GetFeatureName(item.Key);
+                        if (this.flipper.TryIsOn(featureName, item.Value, out isOn) && !isOn)
+                        {
+                            context.BuildKey = new NamedTypeBuildKey(item.Key, item.Value);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            base.PreBuildUp(context);
+        }
+    }
+}
---
+++ test/FeatureFlipper.Unity.Tests/VersionSelectionStrategyFixture.cs
@@ -0,0 +1,119 @@
+﻿namespace FeatureFlipper.Unity.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Practices.ObjectBuilder2;
+    using Moq;
+    using Xunit;
+
+    public class VersionSelectionStrategyFixture
+    {
+        [Fact]
+        public void Ctor_GuardClause()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new VersionSelectionStrategy(null, null));
+            Assert.Throws<ArgumentNullException>(() => new VersionSelectionStrategy(new Mock<IFeatureFlipper>().Object, null));
+        }
+
+        [Fact]
+        public void PreBuildUp_GuardClause()
+        {
+            // Arrange
+            Mock<IFeatureFlipper> flipper = new Mock<IFeatureFlipper>(MockBehavior.Strict);
+            VersionSelectionStrategy strategy = new VersionSelectionStrategy(flipper.Object, new Dictionary<Type, IDictionary<Type, string>>());
+
+            Assert.Throws<ArgumentNullException>(() => strategy.PreBuildUp(null));
+        }
+
+        [Fact]
+        public void PreBuildUp_FeatureOn()
+        {
+            // Arrange
+            bool isOn = true;
+            Mock<IFeatureFlipper> flipper = new Mock<IFeatureFlipper>(MockBehavior.Strict);
+            flipper
+                .Setup(f => f.TryIsOn(It.IsAny<string>(), It.IsAny<string>(), out isOn))
+                .Returns(true);
+
+            IDictionary<Type, IDictionary<Type, string>> featureVersionMapping = new Dictionary<Type, IDictionary<Type, string>>();
+            featureVersionMapping.Add(typeof(IFeature1), new Dictionary<Type, string>() { { typeof(Feature1), "V1" } });
+
+            VersionSelectionStrategy strategy = new VersionSelectionStrategy(flipper.Object, featureVersionMapping);
+
+            Mock<IBuilderContext> context = new Mock<IBuilderContext>();
+            context.SetupAllProperties();
+            context.Object.BuildKey = new NamedTypeBuildKey(typeof(Feature1));
+            context.Setup(c => c.OriginalBuildKey).Returns(new NamedTypeBuildKey(typeof(IFeature1)));
+
+            // Act
+            strategy.PreBuildUp(context.Object);
+
+            // Assert
+            Assert.Null(context.Object.Existing);
+            Assert.NotNull(context.Object.BuildKey);
+            Assert.Equal(typeof(Feature1), context.Object.BuildKey.Type);
+            Assert.Equal(null, context.Object.BuildKey.Name);
+        }
+
+        [Fact]
+        public void PreBuildUp_FeatureOff()
+        {
+            // Arrange
+            bool isOn = false;
+            Mock<IFeatureFlipper> flipper = new Mock<IFeatureFlipper>(MockBehavior.Strict);
+            flipper
+                .Setup(f => f.TryIsOn(It.IsAny<string>(), It.IsAny<string>(), out isOn))
+                .Returns(true);
+
+            IDictionary<Type, IDictionary<Type, string>> featureVersionMapping = new Dictionary<Type, IDictionary<Type, string>>();
+            featureVersionMapping.Add(typeof(IFeature1), new Dictionary<Type, string>() { { typeof(Feature1), "V1" } });
+
+            VersionSelectionStrategy strategy = new VersionSelectionStrategy(flipper.Object, featureVersionMapping);
+
+            Mock<IBuilderContext> context = new Mock<IBuilderContext>();
+            context.SetupAllProperties();
+            context.Object.BuildKey = new NamedTypeBuildKey(typeof(Feature1));
+            context.Setup(c => c.OriginalBuildKey).Returns(new NamedTypeBuildKey(typeof(IFeature1)));
+
+            // Act
+            strategy.PreBuildUp(context.Object);
+
+            // Assert
+            Assert.Null(context.Object.Existing);
+            Assert.NotNull(context.Object.BuildKey);
+            Assert.Equal(typeof(Feature1), context.Object.BuildKey.Type);
+            Assert.Equal("V1", context.Object.BuildKey.Name);
+        }
+
+        [Fact]
+        public void PreBuildUp_FeatureUnknown()
+        {
+            // Arrange
+            bool isOn = false;
+            Mock<IFeatureFlipper> flipper = new Mock<IFeatureFlipper>(MockBehavior.Strict);
+            flipper
+                .Setup(f => f.TryIsOn(It.IsAny<string>(), It.IsAny<string>(), out isOn))
+                .Returns(false);
+
+            IDictionary<Type, IDictionary<Type, string>> featureVersionMapping = new Dictionary<Type, IDictionary<Type, string>>();
+            featureVersionMapping.Add(typeof(IFeature1), new Dictionary<Type, string>() { { typeof(Feature1), "V1" } });
+
+            VersionSelectionStrategy strategy = new VersionSelectionStrategy(flipper.Object, featureVersionMapping);
+
+            Mock<IBuilderContext> context = new Mock<IBuilderContext>();
+            context.SetupAllProperties();
+            context.Object.BuildKey = new NamedTypeBuildKey(typeof(Feature1));
+            context.Setup(c => c.OriginalBuildKey).Returns(new NamedTypeBuildKey(typeof(IFeature1)));
+
+            // Act
+            strategy.PreBuildUp(context.Object);
+
+            // Assert
+            Assert.Null(context.Object.Existing);
+            Assert.NotNull(context.Object.BuildKey);
+            Assert.Equal(typeof(Feature1), context.Object.BuildKey.Type);
+            Assert.Equal(null, context.Object.BuildKey.Name);
+        }
+    }
+}
