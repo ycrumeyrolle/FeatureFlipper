5472ea03bd69a898d142b0c06457e2e8e73ef215
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-08 23:06:25 +0200

    Add helpers methods on ServiceContainer

---
+++ src/FeatureFlipper/ServiceContainer.cs
@@ -0,0 +1,172 @@
+﻿namespace FeatureFlipper
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Linq;
+    using FeatureFlipper.Properties;
+
+    /// <summary>
+    /// Represents the container of services commonly used by FeatureFlipper
+    /// </summary>
+    public class ServiceContainer
+    {
+        private readonly ConcurrentDictionary<Type, object> table = new ConcurrentDictionary<Type, object>();
+        private readonly ConcurrentDictionary<Type, Func<object>> factories = new ConcurrentDictionary<Type, Func<object>>();
+
+        private readonly ConcurrentDictionary<Type, IEnumerable<object>> tableMulti = new ConcurrentDictionary<Type, IEnumerable<object>>();
+        private readonly ConcurrentDictionary<Type, Func<IEnumerable<object>>> factoriesMulti = new ConcurrentDictionary<Type, Func<IEnumerable<object>>>();
+  
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceContainer"/> class.
+        /// </summary>
+        public ServiceContainer()
+        {
+            this.factories[typeof(ISystemClock)] = () => new SystemClock();
+            this.factories[typeof(IConfigurationReader)] = () => new DefaultConfigurationReader();
+            this.factories[typeof(IPrincipalProvider)] = () => new DefaultPrincipalProvider();
+
+            this.factoriesMulti[typeof(IFeatureStateParser)] = () => new List<IFeatureStateParser> 
+            { 
+                new BooleanFeatureStateParser(),
+                new DateFeatureStateParser(this.GetService<ISystemClock>()),
+                new VersionStateParser()
+            };
+
+            this.factories[typeof(IAssembliesResolver)] = () => new DefaultAssembliesResolver();
+            this.factories[typeof(IMetadataProvider)] = () => new DataAnnotationMetadataProvider(this.GetService<ITypeResolver>());
+            this.factories[typeof(ITypeResolver)] = () => new FeatureTypeResolver(this.GetService<IAssembliesResolver>());
+            this.factories[typeof(IRoleMatrixProvider)] = () => new DefaultRoleMatrixProvider();
+            this.factoriesMulti[typeof(IFeatureProvider)] = () => new List<IFeatureProvider> 
+            { 
+                new ConfigurationFeatureProvider(this.GetService<IConfigurationReader>(), this.GetServices<IFeatureStateParser>()),
+                new RoleFeatureProvider(this.GetService<IRoleMatrixProvider>(), this.GetService<IPrincipalProvider>())
+            };
+
+            this.factories[typeof(IFeatureFlipper)] = () => new DefaultFeatureFlipper(this.GetServices<IFeatureProvider>(), this.GetService<IMetadataProvider>());
+        }
+        
+        /// <summary>
+        /// Provides a service instance given a service type.
+        /// </summary>
+        /// <param name="serviceType">The type of the service.</param>
+        /// <returns>The service instance.</returns>
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
+
+            object service;
+            if (this.table.TryGetValue(serviceType, out service))
+            {
+                return service;
+            }
+
+            Func<object> factory;
+            if (this.factories.TryGetValue(serviceType, out factory))
+            {
+                service = factory();
+                this.table.TryAdd(serviceType, service);
+                return service;
+            }
+
+            throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture, Resources.Service_Unsupported, serviceType.Name));
+        }
+
+        /// <summary>
+        /// Provides services instances given a service type.
+        /// </summary>
+        /// <param name="serviceType">The type of the service.</param>
+        /// <returns>The list of the service instances.</returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
+
+            IEnumerable<object> services;
+            if (this.tableMulti.TryGetValue(serviceType, out services))
+            {
+                return services;
+            }
+
+            Func<IEnumerable<object>> factory;
+            if (this.factoriesMulti.TryGetValue(serviceType, out factory))
+            {
+                services = factory();
+                this.tableMulti.TryAdd(serviceType, services);
+                return services;
+            }
+
+            throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture, Resources.Service_Unsupported, serviceType.Name));
+        }
+
+        /// <summary>
+        /// Replaces a service by an other.
+        /// </summary>
+        /// <param name="serviceType">The type of the service to replace.</param>
+        /// <param name="serviceFactory">The <see cref="Func{T}"/> factory creating the service.</param>
+        public void Replace(Type serviceType, Func<object> serviceFactory)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
+
+            if (serviceFactory == null)
+            {
+                throw new ArgumentNullException("serviceFactory");
+            }
+
+            object service;
+            if (!this.table.TryRemove(serviceType, out service) && !this.factories.ContainsKey(serviceType))
+            {
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Service_Invalid, serviceType.Name), "serviceType");
+            }
+
+            this.factories[serviceType] = serviceFactory;
+        }
+
+        /// <summary>
+        /// Replaces a service collection by an other.
+        /// </summary>
+        /// <param name="serviceType">The type of the service to replace.</param>
+        /// <param name="serviceFactories">The <see cref="Func{T}"/> factory creating the services.</param>
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Func<> pattern.")]
+        public void Replace(Type serviceType, Func<IEnumerable<object>> serviceFactories)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
+
+            if (serviceFactories == null)
+            {
+                throw new ArgumentNullException("serviceFactories");
+            }
+
+            if (!this.factoriesMulti.ContainsKey(serviceType))
+            {
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Service_Invalid, serviceType.Name), "serviceType");
+            }
+
+            IEnumerable<object> services;
+            this.tableMulti.TryRemove(serviceType, out services);
+            this.factoriesMulti[serviceType] = serviceFactories;
+        }
+        
+        /// <summary>
+        /// Removes all services collection of a given type.
+        /// </summary>
+        /// <param name="serviceType">The type of the service to remove.</param>
+        public void RemoveAll(Type serviceType)
+        {
+            this.Replace(serviceType, () => new object[0]);
+        }        
+    }
+}
---
+++ src/FeatureFlipper/ServiceProviderExtensions.cs
@@ -0,0 +1,173 @@
+﻿namespace FeatureFlipper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="ServiceContainer"/>.
+    /// </summary>
+    public static class ServiceProviderExtensions
+    {   
+        /// <summary>
+        /// Provides a service instance given a service type.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <returns>The service instance.</returns>
+        public static TService GetService<TService>(this ServiceContainer serviceContainer)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            Type serviceType = typeof(TService);
+            return (TService)serviceContainer.GetService(serviceType);
+        }
+
+        /// <summary>
+        /// Provides services instances instance given a service type.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <returns>The list of the service instances.</returns>
+        public static IEnumerable<TService> GetServices<TService>(this ServiceContainer serviceContainer)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            Type serviceType = typeof(TService);
+            return serviceContainer.GetServices(serviceType).Cast<TService>();
+        }
+      
+        /// <summary>
+        /// Replaces a service collection by an other.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="serviceType">The type of the service to replace.</param>
+        /// <param name="services">The services instances.</param>
+        public static void Replace(this ServiceContainer serviceContainer, Type serviceType, IEnumerable<object> services)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            if (services == null)
+            {
+                throw new ArgumentNullException("services");
+            }
+
+            serviceContainer.Replace(serviceType, () => services);
+        }
+
+        /// <summary>
+        /// Replaces a service by an other.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="serviceType">The type of the service to replace.</param>
+        /// <param name="service">The service instance.</param>
+        public static void Replace(this ServiceContainer serviceContainer, Type serviceType, object service)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            if (service == null)
+            {
+                throw new ArgumentNullException("service");
+            }
+
+            serviceContainer.Replace(serviceType, () => service);
+        }
+        
+        /// <summary>
+        /// Adds a service to a service collection.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="serviceType">The type of the service to replace.</param>
+        /// <param name="service">The service instance.</param>
+        /// <remarks>
+        /// <see cref="IFeatureStateParser"/> and <see cref="IFeatureProvider"/> are the only possible types.
+        /// </remarks>
+        public static void Add(this ServiceContainer serviceContainer, Type serviceType, object service)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            if (service == null)
+            {
+                throw new ArgumentNullException("service");
+            }
+
+            var services = serviceContainer.GetServices(serviceType);
+
+            serviceContainer.Replace(serviceType, () => services.Concat(new[] { service }));
+        }
+
+        /// <summary>
+        /// Adds a service to a service collection.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="service">The service instance.</param>
+        /// <typeparam name="TService">The service type</typeparam>
+        /// <remarks>
+        /// <see cref="IFeatureStateParser"/> and <see cref="IFeatureProvider"/> are the only possible types.
+        /// </remarks>
+        public static void Add<TService>(this ServiceContainer serviceContainer, TService service)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            if (service == null)
+            {
+                throw new ArgumentNullException("service");
+            }
+
+            serviceContainer.Add(typeof(TService), service);
+        }
+
+        /// <summary>
+        /// Adds a <see cref="IFeatureStateParser"/> to the service collection.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="featureStateParser">The service instance.</param>
+        public static void AddFeatureStateParser(this ServiceContainer serviceContainer, IFeatureStateParser featureStateParser)
+        {
+            serviceContainer.Add(featureStateParser);
+        }
+
+        /// <summary>
+        /// Adds a <see cref="IFeatureProvider"/> to the service collection.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <param name="featureProvider">The service instance.</param>
+        public static void AddFeatureProvider(this ServiceContainer serviceContainer, IFeatureProvider featureProvider)
+        {
+            serviceContainer.Add(featureProvider);
+        }
+
+        /// <summary>
+        /// Removes all services collection of a given type.
+        /// </summary>
+        /// <param name="serviceContainer">The <see cref="ServiceContainer"/></param>
+        /// <typeparam name="TService">The type of the service to remove.</typeparam>
+        public static void RemoveAll<TService>(this ServiceContainer serviceContainer)
+        {
+            if (serviceContainer == null)
+            {
+                throw new ArgumentNullException("serviceContainer");
+            }
+
+            serviceContainer.RemoveAll(typeof(TService));
+        }
+    }
+}
