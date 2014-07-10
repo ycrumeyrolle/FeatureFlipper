97ab3ab9d0f4ee23d524d75cb3d63c473f96a65b
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-10 21:28:45 +0200

    update xunit.sh

--- xunit.sh
+++ xunit.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -x
 
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-build2700 -o packages
+mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-build2700 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe $@ -xml coverage.xml
+   mv packages/xunit.core.2.0.0-beta-build2700/tools/*.dll packages/xunit.runners.2.0.0-beta-build2700/tools/
+   mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe $@ -xml coverage.xml
    if [ $? -ne 0 ]
    then   
      exit 1
