ef001f289bae91fb508a39807cce67a23f0cbcb9
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-12 15:12:39 +0200

    update xunit.sh

--- xunit.sh
+++ xunit.sh
@@ -4,10 +4,11 @@ mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-b
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-build2700 -o packages
 
 runTest(){
+   cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./
    cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./
    cp $@* ./
    
-   mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe ./FeatureFlipper.Tests.dll -xml coverage.xml
+   mono --runtime=v4.0 xunit.console.exe ./FeatureFlipper.Tests.dll -xml coverage.xml
    if [ $? -ne 0 ]
    then   
      exit 1
