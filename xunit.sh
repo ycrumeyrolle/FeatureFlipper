c17d517f9ad550515399684920ed65650fb9f393
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-12 14:03:04 +0200

    update travis files

--- xunit.sh
+++ xunit.sh
@@ -5,7 +5,7 @@ mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-buil
 
 runTest(){
    cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./
-   cp $@ ./
+   cp $@* ./
    
    mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe ./FeatureFlipper.Tests.dll -xml coverage.xml
    if [ $? -ne 0 ]
@@ -16,4 +16,4 @@ runTest(){
 
 runTest $1
 
-exit $?
+exit test/FeatureFlipper.Tests/bin/Debug/
