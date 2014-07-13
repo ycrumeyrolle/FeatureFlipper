2184d49cd6bfc2865ca54dfbcbb8aa07c1e5aa5c
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-13 15:46:12 +0200

    update xunit.sh

--- xunit.sh
+++ xunit.sh
@@ -4,17 +4,20 @@ mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-b
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-build2700 -o packages
 
 runTest(){
-   cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./
-   cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./
-   cp $@* ./
+   md build
+   cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./build
+   cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./build
+   cp $1* ./build
    
-   mono --runtime=v4.0 xunit.console.exe ./FeatureFlipper.Tests.dll -xml coverage.xml
+   mono --runtime=v4.0 xunit.console.exe ./build/$2
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest test/FeatureFlipper.Tests/bin/Debug/
+runTest test/FeatureFlipper.Tests/bin/Debug/ FeatureFlipper.Tests.dll
+runTest test/FeatureFlipper.Unity.Tests/bin/Debug/ FeatureFlipper.Unity.Tests.dll
+runTest test/FeatureFlipper.WebApi.Tests/bin/Debug/ FeatureFlipper.WebApi.Tests.dll
 
 exit 
