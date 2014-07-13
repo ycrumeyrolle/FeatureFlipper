b66759f3b26d1f26c4925fcb31c92356a6798ad8
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-13 16:00:19 +0200

    update xunit.sh

--- xunit.sh
+++ xunit.sh
@@ -2,14 +2,15 @@
 
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-build2700 -o packages
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-build2700 -o packages
+mkdir build
+cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./build
+cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./build
 
 runTest(){
-   mkdir build
-   cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./build
-   cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./build
+
    cp $1* ./build
    
-   mono --runtime=v4.0 xunit.console.exe ./build/$2
+   mono --runtime=v4.0 ./build/xunit.console.exe ./build/$2
    if [ $? -ne 0 ]
    then   
      exit 1
