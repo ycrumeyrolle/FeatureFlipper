3bc6212787ffee6a34705ae792d6d8b271926d29
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-26 09:58:26 +0200

    add missing test

---
+++ test/FeatureFlipper.Tests/CycleDetection/VertexTests.cs
@@ -0,0 +1,40 @@
+﻿namespace FeatureFlipper.Tests.CycleDetection
+{
+    using System;
+    using FeatureFlipper.CycleDetection;
+    using Xunit;
+
+    public class VertexTests
+    {
+        [Fact]
+        public void Ctor_GuardClause()
+        {
+            // Act & assert
+            Assert.Throws<ArgumentNullException>(() => new Vertex("test", null));
+            Assert.DoesNotThrow(() => new Vertex(null));
+            Assert.DoesNotThrow(() => new Vertex(null, new Vertex[0]));
+        }
+
+        [Fact]
+        public void Ctor1()
+        {
+            // Act
+            Vertex vertex = new Vertex("test");
+
+            // Assert
+            Assert.Equal("test", vertex.Value);
+            Assert.Equal(0, vertex.Dependencies.Count);
+        }
+
+        [Fact]
+        public void Ctor2()
+        {
+            // Act
+            Vertex vertex = new Vertex("test", new[] { new Vertex("1"), new Vertex("2") });
+
+            // Assert
+            Assert.Equal("test", vertex.Value);
+            Assert.Equal(2, vertex.Dependencies.Count);
+        }
+    }
+}
